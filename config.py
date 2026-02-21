151b61271fb1f5017d2785cfb7a52e543d0dc301
Ayngaran <ayngarannn@gmail.com>
2026-02-22 01:29:32 +0530

    Added configutation to avoid package/dependency releated files

--- config.py
+++ config.py
@@ -8,3 +8,7 @@ MODEL_NAME = os.getenv("MODEL_NAME", "gemini-2.5-flash")
 MAX_FILE_SIZE_KB = 100
 MAX_TOKENS = 4000
 SUPPORTED_EXTENSIONS = {".py", ".js", ".java"}
+EXCLUDED_DIRS = {             
+    "node_modules", ".git", "__pycache__",
+    "venv", ".venv", "dist", "build", ".next"
+}
