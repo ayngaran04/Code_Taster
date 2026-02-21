dad5e18853f527eb17329938eee26cf2c1e69db7
Ayngaran <ayngarannn@gmail.com>
2026-02-21 16:44:24 +0530

    Added dependencies and defined the state

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-def main():
-    print("Hello from researcher!")
+import operator
+from langchain_core.messages import BaseMessage, HumanMessage
+from typing import Annotated, List, TypedDict, Union
+from langgraph.graph import StateGraph, START, END
+
+class TeamState(TypedDict):
+    messages: Annotated[List[BaseMessage], operator.add]
+    next: str   
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
