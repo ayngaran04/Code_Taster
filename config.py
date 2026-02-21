1fa5b22bc0bb42d8c47a2ce03e434ba3df633152
Ayngaran <ayngarannn@gmail.com>
2026-02-22 01:27:08 +0530

    added config files to initialize the llm

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+MODEL_NAME = os.getenv("MODEL_NAME", "gemini-2.5-flash")
+MAX_FILE_SIZE_KB = 100
+MAX_TOKENS = 4000
+SUPPORTED_EXTENSIONS = {".py", ".js", ".java"}
