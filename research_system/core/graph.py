58cbb7eae22f12dd2efc92e8f088e757f15e40d2
Ayngaran <ayngarannn@gmail.com>
2026-02-23 01:51:25 +0530

    addded graph file and nodes

---
+++ research_system/core/graph.py
@@ -0,0 +1,22 @@
+from langgraph.graph import StateGraph, END
+from core.state import ReviewState
+from agents.security_agent import run_security_agent
+from agents.architeture_agent import run_architecture_agent
+from agents.code_quality_agent import run_code_quality_agent
+from agents.outputText import run_synthesizer_agent
+
+def build_graph() -> StateGraph:
+    graph = StateGraph(ReviewState)
+
+
+    graph.add_node("security", run_security_agent)
+    graph.add_node("architecture", run_architecture_agent)
+    graph.add_node("code_quality", run_code_quality_agent)
+    graph.add_node("synthesizer", run_synthesizer_agent)
+
+    graph.add_edge("security", "synthesizer") 
+    graph.add_edge("architecture", "synthesizer")
+    graph.add_edge("code_quality", "synthesizer")
+    graph.add_edge("synthesizer", END)
+
+    return graph.compile()
