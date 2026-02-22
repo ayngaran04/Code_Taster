5707f26cc02d52d3f48eb1330ca8208b84756b0f
Ayngaran <ayngarannn@gmail.com>
2026-02-23 00:03:40 +0530

    added code quality agent

---
+++ research_system/agents/code_quality_agent.py
@@ -0,0 +1,25 @@
+from core.state import ReviewState, AgentFinding
+from config import MODEL_NAME,MAX_TOKENS
+from langchain_google_genai import GoogleGenerativeAI
+from langchain.messages import HumanMessage
+from agents.security_agent import format_files_for_prompt, load_prompt, parse_llm_responses
+
+def run_code_quality_agent(state:ReviewState)-> ReviewState:
+    llm  = GoogleGenerativeAI(model=MODEL_NAME, max_tokens=MAX_TOKENS, temperature=0.5)
+    prompt_template =  load_prompt("code_quality")
+    file_content = format_files_for_prompt(state)
+    prompt = prompt_template.replace("{file_contents}", file_content)
+
+    try:
+        response = llm.invoke([HumanMessage(content=prompt)])
+        findings, errors = parse_llm_responses(response.content, "code_quality")
+    except Exception as e:
+        findings = []
+        errors = [f"Code Quality agent failed: {str(e)}"]
+
+    return {
+        **state,  
+        "code_quality_findings": findings,
+        "errors": state["errors"] + errors
+    }
+
